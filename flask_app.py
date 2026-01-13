79a89ee48752e48ca14930c159f2cd00d8a4653b
pravan82301487 <pranav.rama09@gmail.com>
2026-01-13 07:58:39 +0100

    Update flask_app.py
    ok help

--- flask_app.py
+++ flask_app.py
@@ -10,13 +10,13 @@ from flask_login import login_user, logout_user, login_required, current_user
 import logging
 
 logging.basicConfig(
-level=logging.DEBUG,
-format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+    level=logging.DEBUG,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
 )
 
 # Load .env variables
 load_dotenv()
-W_SECRET = os.getenv("W_SECRET") 
+W_SECRET = os.getenv("W_SECRET")
 
 # Init flask app
 app = Flask(__name__)
@@ -29,82 +29,82 @@ login_manager.login_view = "login"
 
 # DON'T CHANGE
 def is_valid_signature(x_hub_signature, data, private_key):
-hash_algorithm, github_signature = x_hub_signature.split('=', 1)
-algorithm = hashlib.__dict__.get(hash_algorithm)
-encoded_key = bytes(private_key, 'latin-1')
-mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
-return hmac.compare_digest(mac.hexdigest(), github_signature)
+    hash_algorithm, github_signature = x_hub_signature.split('=', 1)
+    algorithm = hashlib.__dict__.get(hash_algorithm)
+    encoded_key = bytes(private_key, 'latin-1')
+    mac = hmac.new(encoded_key, msg=data, digestmod=algorithm)
+    return hmac.compare_digest(mac.hexdigest(), github_signature)
 
 # DON'T CHANGE
 @app.post('/update_server')
 def webhook():
-x_hub_signature = request.headers.get('X-Hub-Signature')
-if is_valid_signature(x_hub_signature, request.data, W_SECRET):
-repo = git.Repo('./mysite')
-origin = repo.remotes.origin
-origin.pull()
-return 'Updated PythonAnywhere successfully', 200
-return 'Unathorized', 401
+    x_hub_signature = request.headers.get('X-Hub-Signature')
+    if is_valid_signature(x_hub_signature, request.data, W_SECRET):
+        repo = git.Repo('./mysite')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Updated PythonAnywhere successfully', 200
+    return 'Unathorized', 401
 
 # Auth routes
 @app.route("/login", methods=["GET", "POST"])
 def login():
-error = None
+    error = None
 
-if request.method == "POST":
-user = authenticate(
-request.form["username"],
-request.form["password"]
-)
+    if request.method == "POST":
+        user = authenticate(
+            request.form["username"],
+            request.form["password"]
+        )
 
-if user:
-login_user(user)
-return redirect(url_for("index"))
+        if user:
+            login_user(user)
+            return redirect(url_for("index"))
 
-error = "Benutzername oder Passwort ist falsch."
+        error = "Benutzername oder Passwort ist falsch."
 
-return render_template(
-"auth.html",
-title="In dein Konto einloggen",
-action=url_for("login"),
-button_label="Einloggen",
-error=error,
-footer_text="Noch kein Konto?",
-footer_link_url=url_for("register"),
-footer_link_label="Registrieren"
-)
+    return render_template(
+        "auth.html",
+        title="In dein Konto einloggen",
+        action=url_for("login"),
+        button_label="Einloggen",
+        error=error,
+        footer_text="Noch kein Konto?",
+        footer_link_url=url_for("register"),
+        footer_link_label="Registrieren"
+    )
 
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-error = None
+    error = None
 
-if request.method == "POST":
-username = request.form["username"]
-password = request.form["password"]
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
 
-ok = register_user(username, password)
-if ok:
-return redirect(url_for("login"))
+        ok = register_user(username, password)
+        if ok:
+            return redirect(url_for("login"))
 
-error = "Benutzername existiert bereits."
+        error = "Benutzername existiert bereits."
 
-return render_template(
-"auth.html",
-title="Neues Konto erstellen",
-action=url_for("register"),
-button_label="Registrieren",
-error=error,
-footer_text="Du hast bereits ein Konto?",
-footer_link_url=url_for("login"),
-footer_link_label="Einloggen"
-)
+    return render_template(
+        "auth.html",
+        title="Neues Konto erstellen",
+        action=url_for("register"),
+        button_label="Registrieren",
+        error=error,
+        footer_text="Du hast bereits ein Konto?",
+        footer_link_url=url_for("login"),
+        footer_link_label="Einloggen"
+    )
 
 @app.route("/logout")
 @login_required
 def logout():
-logout_user()
-return redirect(url_for("index"))
+    logout_user()
+    return redirect(url_for("index"))
 
 
 
@@ -112,24 +112,23 @@ return redirect(url_for("index"))
 @app.route("/", methods=["GET", "POST"])
 @login_required
 def index():
-# GET
-if request.method == "GET":
-todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
-return render_template("main_page.html", todos=todos)
+    # GET
+    if request.method == "GET":
+        todos = db_read("SELECT id, content, due FROM todos WHERE user_id=%s ORDER BY due", (current_user.id,))
+        return render_template("main_page.html", todos=todos)
 
-# POST
-content = request.form["contents"]
-due = request.form["due_at"]
-    db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s)", (current_user.id, content, due, ))
+    # POST
+    content = request.form["contents"]
+    due = request.form["due_at"]
     db_write("INSERT INTO todos (user_id, content, due) VALUES (%s, %s, %s)", (current_user.id, content, due, ))
-return redirect(url_for("index"))
+    return redirect(url_for("index"))
 
 @app.post("/complete")
 @login_required
 def complete():
-todo_id = request.form.get("id")
-db_write("DELETE FROM todos WHERE user_id=%s AND id=%s", (current_user.id, todo_id,))
-return redirect(url_for("index"))
+    todo_id = request.form.get("id")
+    db_write("DELETE FROM todos WHERE user_id=%s AND id=%s", (current_user.id, todo_id,))
+    return redirect(url_for("index"))
 
 if __name__ == "__main__":
-app.run()
+    app.run()
